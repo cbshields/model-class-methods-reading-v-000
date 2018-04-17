24f2d665104244a7cdbf39790e82a087ead45b51
Caroline Shields <cbshields@outlook.com>
2018-04-17 20:09:53 +0000

    created Post date methods, added to controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,14 @@ class Post < ActiveRecord::Base
     where(author: author_id)
   end
 
+  def self.from_today
+    @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+
+  def self.old_news
+    @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+  end
+
   private
 
   def is_title_case
